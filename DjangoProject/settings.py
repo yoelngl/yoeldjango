7ea01fe0568dc06f8a1cbc18496d06f7895b59e1
Janice Chew <janice_czh@yahoo.com.sg>
2020-08-26 17:56:51 +0700

    -

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -77,20 +77,12 @@ WSGI_APPLICATION = 'DjangoProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'django_website',
-#         'USER': 'root',
-#         'PASSWORD': '',
-#         'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-#         },
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 DATABASES['default'].update(prod_db)
 
